4837d8fd68f3af43ff3f5786b05413489735a700
Hosameldin <hosameldin.mohamed@iit.it>
2022-10-23 16:31:19 +0200

    LazyGit key shortcut

--- lua/hosameldin/core/keymaps.lua
+++ lua/hosameldin/core/keymaps.lua
@@ -40,3 +40,7 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+
+-- LazyGit
+keymap.set("n", "<leader>gg", ":LazyGit<CR>") -- Open LazyGit
+
