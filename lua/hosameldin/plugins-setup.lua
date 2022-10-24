7a4894687c2ff69a56513a5b1c52f50bf26ea93f
Hosameldin <hosameldin.mohamed@iit.it>
2022-10-24 15:46:51 +0200

    Add winbar

--- lua/hosameldin/plugins-setup.lua
+++ lua/hosameldin/plugins-setup.lua
@@ -96,6 +96,9 @@ return packer.startup(function(use)
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
     use("kdheepak/lazygit.nvim")
 
+    -- Winbar
+    use("fgheng/winbar.nvim")
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
