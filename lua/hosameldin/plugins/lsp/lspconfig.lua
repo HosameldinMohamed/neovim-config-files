16fc6b0e0b3290373d9a28b7320eb54bf777d109
Hosameldin <hosameldin.mohamed@iit.it>
2023-03-06 18:13:59 +0100

    remove deprecated sumneko_lua from LSP conf

--- lua/hosameldin/plugins/lsp/lspconfig.lua
+++ lua/hosameldin/plugins/lsp/lspconfig.lua
@@ -138,7 +138,7 @@ configs["qml_lsp"] = {
 lspconfig["qml_lsp"].setup({})
 
 -- configure lua server (with special settings)
-lspconfig["sumneko_lua"].setup({
+lspconfig["lua_ls"].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 	settings = { -- custom settings for lua
