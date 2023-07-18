fca45ed7bd4e05f9e8375e1651e8d10b11fee83c
Hosameldin <hosameldin.mohamed@iit.it>
2023-07-18 16:04:31 +0200

    nvimtree set width=50

--- lua/hosameldin/plugins/nvim-tree.lua
+++ lua/hosameldin/plugins/nvim-tree.lua
@@ -14,6 +14,9 @@ vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 -- configure nvim-tree
 nvimtree.setup({
 	-- change folder arrow icons
+    view = {
+    width = 50,
+    },
 	renderer = {
 		icons = {
 			glyphs = {
@@ -54,7 +57,7 @@ nvimtree.setup({
 			window_picker = {
 				enable = false,
 			},
-            resize_window = false;
+            resize_window = true;
 		},
 	},
 })
