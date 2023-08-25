1d78121751a984ab300543d5e50c5bbcb1bdcd10
Hosameldin <hosameldin.mohamed@iit.it>
2023-08-25 05:03:34 +0200

    add noice notify at the status line

--- lua/hosameldin/plugins/lualine.lua
+++ lua/hosameldin/plugins/lualine.lua
@@ -44,6 +44,10 @@ lualine.setup {
                 unnamed = icons.ui.Question,
             },
         }, },
+        lualine_x = {{ require("noice").api.statusline.mode.get,
+        cond = require("noice").api.statusline.mode.has,
+        color = { fg = "#ff9e64" }, },
+        'encoding', 'fileformat', 'filetype'},
     },
     winbar = {
         lualine_a = { "mode" },
