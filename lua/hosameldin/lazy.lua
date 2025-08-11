e564a280d6fd5bec6ad0325c713ae1ad533f3fdd
Hosameldin <hosameldin.mohamed@iit.it>
2025-08-11 16:24:05 +0200

    update markdown-preview

--- lua/hosameldin/lazy.lua
+++ lua/hosameldin/lazy.lua
@@ -120,11 +120,15 @@ require('lazy').setup({
         dependencies = 'neovim/nvim-lspconfig'
     },
 
-    -- for Markdown preview
-    -- install without yarn or npm
+    -- install with yarn or npm
     {
-        'iamcco/markdown-preview.nvim',
-        build = function() vim.fn['mkdp#util#install']() end,
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
     },
 
     -- Markdown TOC generation
